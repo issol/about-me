9cfb75ba39136c3a10a3ba2a3cb56f27c0f1f4dd
issol <isolatorv@gmail.com>
2021-04-14 23:41:43 +0900

    feat / first display

---
+++ src/component/MainContent/container.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import MainContentPresenter from './presenter';
+
+const MainContentContainer = () => {
+  return <MainContentPresenter />;
+};
+
+export default MainContentContainer;
