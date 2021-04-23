3b922f06aa619f692f45307f9d84ecc006880125
issol <isolatorv@gmail.com>
2021-04-23 21:14:31 +0900

    feat / go Top Button function

--- src/component/SelfInterview/container.tsx
+++ src/component/SelfInterview/container.tsx
@@ -2,8 +2,8 @@ import React, { useEffect, useRef, useState } from 'react';
 import SelfInterviewPresenter from './presenter';
 
 export type SelfInterViewType = {
-  questions: { question: string; isActive: boolean }[];
-  answers: { answer: string }[];
+  questions: { id: number; question: string; isActive: boolean }[];
+  answers: { id: number; answer: string }[];
 };
 
 const SelfInterviewContainer = () => {
@@ -12,11 +12,15 @@ const SelfInterviewContainer = () => {
   const [isActive, setIsActive] = useState(false);
   const [selfInterview, setSelfInterview] = useState<SelfInterViewType>({
     questions: [
-      { question: '언제부터 지금의 직무에 관심을 갖게 되었나요?', isActive: false },
-      { question: '일에 있어가장 중요하게 생각하는 것이 있다면 ?', isActive: false },
-      { question: '학창시절의 경험 중 가장 멋진 경험을 들려주세요.', isActive: false },
+      { id: 1, question: '언제부터 지금의 직무에 관심을 갖게 되었나요?', isActive: false },
+      { id: 2, question: '일에 있어가장 중요하게 생각하는 것이 있다면 ?', isActive: false },
+      { id: 3, question: '학창시절의 경험 중 가장 멋진 경험을 들려주세요.', isActive: false },
     ],
-    answers: [{ answer: '1dd' }, { answer: '2dd' }, { answer: '3dd' }],
+    answers: [
+      { id: 1, answer: '1dd' },
+      { id: 2, answer: '2dd' },
+      { id: 3, answer: '3dd' },
+    ],
   });
 
   const handleListDropDown = (idx: number) => {
